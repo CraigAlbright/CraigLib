7773a9454b00c98f76047638f08620518a116c14
Craig Albright <Craig.Albright@gmail.com>
2014-10-29 10:25:48 -0500

    updated dependency injection to register all types

--- CraigLib.Main/DependencyInjectionHelper.cs
+++ CraigLib.Main/DependencyInjectionHelper.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Autofac;
+﻿using Autofac;
 using CraigLib.Cloud.AWS;
+using CraigLib.Cloud.Azure;
+using CraigLib.File;
+using CraigLib.Sound;
+using CraigLib.TimeAndDate;
 
 namespace CraigLib
 {
@@ -15,6 +14,10 @@ namespace CraigLib
         {
             var builder = new ContainerBuilder();
             builder.RegisterType<AWSHelper>().As<IAWSHelper>();
+            builder.RegisterType<AzureHelper>().As<IAzureHelper>();
+            builder.RegisterType<FileHelper>().As<IFileHelper>();
+            builder.RegisterType<TimeAndDateHelper>().As<ITimeAndDateHelper>();
+            builder.RegisterType<LibsndfileApi>().As<ILibsndfileApi>();
             Container = builder.Build();
 
         }
