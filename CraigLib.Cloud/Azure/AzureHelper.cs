af2b391d6c1760b9b58f79b59026ae9a58ac836e
Craig Albright <Craig.Albright@gmail.com>
2014-10-29 14:47:12 -0500

    updated

--- CraigLib.Cloud/Azure/AzureHelper.cs
+++ CraigLib.Cloud/Azure/AzureHelper.cs
@@ -1,12 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Management;
+using Microsoft.WindowsAzure.Management.Compute;
+using Microsoft.WindowsAzure.Management.Sql;
+using Microsoft.WindowsAzure.Management.Storage;
+using Microsoft.WindowsAzure.Management.TrafficManager;
+using Microsoft.WindowsAzure.Management.WebSites;
 
 namespace CraigLib.Cloud.Azure
 {
     public class AzureHelper : IAzureHelper
     {
+        private readonly SubscriptionCloudCredentials _credentials;
+
+        public AzureHelper()
+        {
+            var config = new AzureConfiguration();
+            var token = config.AzureToken;
+            _credentials = new TokenCloudCredentials(token);
+
+        }
+        public ManagementClient GetManagementClient()
+        {
+            return new ManagementClient(_credentials);
+        }
+
+        public ComputeManagementClient GetComputeManagementClient()
+        {
+            return new ComputeManagementClient(_credentials);
+        }
+
+        public SqlManagementClient GetSqlManagementClient()
+        {
+            return new SqlManagementClient(_credentials);
+        }
+
+        public StorageManagementClient GetStorageManagementClient()
+        {
+            return new StorageManagementClient(_credentials);
+        }
+
+        public TrafficManagerManagementClient GeTrafficManagerManagementClient()
+        {
+            return new TrafficManagerManagementClient(_credentials);
+        }
+
+        public WebSiteManagementClient GetWebSiteManagementClient()
+        {
+            return new WebSiteManagementClient(_credentials);
+        }
     }
 }
--- CraigLib.Cloud/Azure/IAzureHelper.cs
+++ CraigLib.Cloud/Azure/IAzureHelper.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.WindowsAzure.Management;
+using Microsoft.WindowsAzure.Management.Compute;
+using Microsoft.WindowsAzure.Management.Sql;
+using Microsoft.WindowsAzure.Management.Storage;
+using Microsoft.WindowsAzure.Management.TrafficManager;
+using Microsoft.WindowsAzure.Management.WebSites;
 
 namespace CraigLib.Cloud.Azure
 {
     public interface IAzureHelper
     {
+        ManagementClient GetManagementClient();
+        ComputeManagementClient GetComputeManagementClient();
+        SqlManagementClient GetSqlManagementClient();
+        StorageManagementClient GetStorageManagementClient();
+        TrafficManagerManagementClient GeTrafficManagerManagementClient();
+        WebSiteManagementClient GetWebSiteManagementClient();
     }
 }
