ff6080b477d78aacdcf05fead142fd5e0fd29d4a
Craig Albright <Craig.Albright@gmail.com>
2014-10-29 15:24:50 -0500

    publish

---
+++ CraigLib.Cloud/AzureConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using System.Configuration;
+
+namespace CraigLib.Cloud
+{
+    public class AzureConfiguration : ConfigurationSection
+    {
+        [ConfigurationProperty("AzureProfileName", DefaultValue = "development", IsRequired = true)]
+        [StringValidator(InvalidCharacters = "~!@#$%^&*()[]{}/;'\"|\\", MinLength = 1, MaxLength = 60)]
+        public string AzureProfileName
+        {
+            get
+            {
+                return (string)this["AzureProfileName"];
+            }
+            set
+            {
+                this["name"] = value;
+            }
+        }
+
+        [ConfigurationProperty("AzureToken", DefaultValue = "", IsRequired = true)]
+        [StringValidator(InvalidCharacters = "~!@#$%^&*()[]{}/;'\"|\\", MinLength = 1, MaxLength = 60)]
+        public string AzureToken
+        {
+            get
+            {
+                return (string)this["AzureToken"];
+            }
+            set
+            {
+                this["name"] = value;
+            }
+        }
+    }
+}
